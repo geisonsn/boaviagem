ce93ceb3865e37973ea37e604e924a82dd49a9c7
Geison Santos <geisons.sn@gmail.com>
2015-04-28 07:36:04 -0400

    Implementação parcial do padrão DAO

--- app/src/main/java/br/com/casadocodigo/boaviagem/dao/BoaViagemDAO.java
+++ app/src/main/java/br/com/casadocodigo/boaviagem/dao/BoaViagemDAO.java
@@ -1,9 +1,15 @@
 package br.com.casadocodigo.boaviagem.dao;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import br.com.casadocodigo.boaviagem.DatabaseHelper;
+import br.com.casadocodigo.boaviagem.domain.Viagem;
 
 /**
  * Created by p001234 on 27/04/15.
@@ -28,13 +34,26 @@ public class BoaViagemDAO {
         helper.close();
     }
 
-    /*public List<Viagem> listViagem() {
+    public List<Viagem> listarViagem() {
         Cursor cursor = getDb().query(DatabaseHelper.Viagem.TABELA,
                 DatabaseHelper.Viagem.COLUNAS,
                 null, null, null, null, null);
         List<Viagem> viagens = new ArrayList<>();
         while (cursor.moveToNext()) {
             Viagem viagem = criarViagem(cursor);
+            viagens.add(viagem);
         }
-    }*/
+        return viagens;
+    }
+
+    private Viagem criarViagem(Cursor cursor) {
+        Viagem viagem = new Viagem();
+        viagem.setId(cursor.getLong(0));
+        viagem.setTipoViagem(cursor.getInt(1));
+        viagem.setDestino(cursor.getString(2));
+        viagem.setDataChegada(new Date(cursor.getLong(3)));
+        viagem.setDataSaida(new Date(cursor.getLong(4)));
+        viagem.setOrcamento(cursor.getDouble(5));
+        return viagem;
+    }
 }
